7b29d29b4f7710abd6e18740beff05a025cfefdd
Mark <marktalamson@gmail.com>
2024-02-27 14:31:21 +0300

    styling

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var qrcode = new QRCode(document.getElementById("qrcode"), {
-    width: 500,
-    height: 500,
+    width: 300,
+    height: 300,
     colorDark: "#000000",
     colorLight: "#ffffff",
     correctLevel: QRCode.CorrectLevel.H
   });
 
   // Set the data (e.g., a URL or any text)
-  var data = "https://emtee-1023.github.io/social-media-links/";
+  var data = "https://emtee-1023.github.io/portfolio/";
   qrcode.makeCode(data);
